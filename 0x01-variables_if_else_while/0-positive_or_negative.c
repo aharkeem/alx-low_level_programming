b27909e8dfdd735da2125d6e0c99fb7396adcd07
aharkeem <ayanbadeakeem@gmail.com>
2022-12-12 08:54:43 -0800

    task 2

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,18 +11,16 @@
  * Return: Always 0 (Success)
  *
  */
-int main(void)
+ int main(void)
 
-{
+ {
 
 	int n;
-
 
 
         srand(time(0));
 
 	n = rand() - RAND_MAX / 2;
-
 
 
         if (n > 0)
@@ -50,4 +48,4 @@ int main(void)
 
 	return (0);
 
-}
+ }   
