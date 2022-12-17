f07e04b5accf034c8f304b4b2976bae178818543
aharkeem <ayanbadeakeem@gmail.com>
2022-12-17 03:33:00 -0800

    task 2

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,15 +6,10 @@
  */
 
 void times_table(void)
-
 {
-
 int a, b, c;
-
 for (a = 0; a <= 9; a++)
-
 {
-
 for (b = 0; b <= 9; b++)
 {
 c = a * b;
@@ -22,44 +17,29 @@ if ((c / 10) == 0)
 {
 if (b == 0)
 {
-
 _putchar('0');
-
 }
 if (b != 0)
 {
-
 _putchar(' ');
 _putchar((c % 10) + '0');
 }
 if (b < 9)
 {
-
 _putchar(',');
 _putchar(' ');
-
 }
 }
 else
-
 {
-
 _putchar((c / 10) + '0');
-
 _putchar((c % 10) + '0');
-
 if (b < 9)
-
 {
-
 _putchar(',');
-
 _putchar(' ');
-
 }
-
 }
-
 }
 _putchar('\n');
 }
