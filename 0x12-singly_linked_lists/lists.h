fe5cfa668b9fca145e55738d7a1302fec51544ee
aharkeem <ayanbadeakeem@gmail.com>
2023-01-26 08:58:18 -0800

    task1

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,30 +1,15 @@
 #ifndef LISTS_H
-
 #define LISTS_H
-
-
-
 #include <stdlib.h>
 
-
-
 /**
- *
- *  * struct list_s - singly linked list
- *
- *   * @str: string - (malloc'ed string)
- *
- *    * @len: length of the string
- *
- *     * @next: points to the next node
- *
- *      *
- *
- *       * Description: singly linked list node structure
- *
- *        *              for project
- *
- *         */
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ * for project
+ */
 
 typedef struct list_s
 
@@ -53,5 +38,3 @@ void free_list(list_t *head);
 
 
 #endif /* LISTS_H */
-
-
